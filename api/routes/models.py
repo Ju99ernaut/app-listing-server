a22d1c41321d4c4a790dd521a597990c527a40c0
Ju99ernaut <brendonzee@gmail.com>
2021-03-27 13:06:19 +0200

    Update models.py

--- api/routes/models.py
+++ api/routes/models.py
@@ -2,7 +2,20 @@ from typing import Optional
 from pydantic import BaseModel
 
 
-class Item(BaseModel):
-    id: Optional[int] = None
-    column1: Optional[str] = None
-    column2: str
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
+
+class User(BaseModel):
+    username: str
+    email: Optional[str] = None
+    disabled: Optional[bool] = None
+
+
+class UserInDB(User):
+    password: str
