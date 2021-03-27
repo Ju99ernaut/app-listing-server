12df64af9b4f8d642e979b52a2489a3e1b50346f
Ju99ernaut <brendonzee@gmail.com>
2021-03-27 15:15:45 +0200

    Update models.py

--- api/routes/models.py
+++ api/routes/models.py
@@ -1,5 +1,23 @@
-from typing import Optional
+from typing import Optional, List
 from pydantic import BaseModel
+
+
+class Application(BaseModel):
+    image: str
+    title: str
+    by: str
+    groups: List(str)
+
+
+class Rating(BaseModel):
+    user: str
+    application: str
+    rating: float
+
+
+class RatingAverage(BaseModel):
+    application: str
+    rating: float
 
 
 class Token(BaseModel):
