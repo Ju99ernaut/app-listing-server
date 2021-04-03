4da1afd00416b868ff8eb1e9d6c318702f1f61f0
Ju99ernaut <brendonzee@gmail.com>
2021-04-03 23:03:32 +0200

    FIx schema collisions

--- api/routes/ratings.py
+++ api/routes/ratings.py
@@ -14,14 +14,14 @@ async def read_ratings():
     return [ratings for ratings in data.get_all_ratings()]
 
 
-@router.get("/{user}", response_model=List[Rating])
+@router.get("/user/{user}", response_model=List[Rating])
 async def read_user_ratings(user: str):
     return [ratings for ratings in data.get_user_ratings(user)]
 
 
-@router.get("/{application}", response_model=List[Rating])
+@router.get("/app/{application}", response_model=List[Rating])
 async def read_app_ratings(application: str):
-    return [ratings for ratings in data.get_appliation_ratings(application)]
+    return [ratings for ratings in data.get_application_ratings(application)]
 
 
 @router.get("/average/{application}", response_model=RatingAverage)
@@ -32,6 +32,6 @@ async def read_app_average(application: str):
     return {"application": application, "rating": rating}
 
 
-@router.get("/average", response_model=List[RatingAverage])
+@router.get("/averages", response_model=List[RatingAverage])
 async def read_all_app_averages():
     return data.get_all_average_ratings()
